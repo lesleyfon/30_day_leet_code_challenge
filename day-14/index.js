3bccf8d2c91447b8c4ec04e9e36d2a55668416a6
lesley <lesleybanadzem2017@gmail.com>
2020-04-14 13:38:37 -0700

    wrote out code for the algorithm

--- day-14/index.js
+++ day-14/index.js
@@ -1,1 +1,21 @@
 
+/**
+ * @param {string} s
+ * @param {number[][]} shift
+ * @return {string}
+ */
+var stringShift = function(s, shift) {
+    let stringArray = s.split("");
+
+    for (let i = 0; i < shift.length; i++) {
+        if(shift[i][0]===0){
+            const copy = stringArray.splice(0, shift[i][1]);
+            stringArray.push(...copy)
+        }else{
+            const copy = stringArray.splice(stringArray.length - shift[i][1], shift[i][1])
+            stringArray.unshift(...copy)
+        }
+    }
+
+    return stringArray.join('')
+};
